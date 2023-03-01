987a1daa9c99d5ea9d4d82d6ec339132db36eac9
Harrison <monteiro4100@gmail.com>
2023-03-01 18:32:42 -0300

    requisito 10

--- app/backend/src/database/controller/user.controller.ts
+++ app/backend/src/database/controller/user.controller.ts
@@ -5,14 +5,18 @@ import login from '../service/user.service';
 const secret = process.env.JWT_SECRET || 'seusecretdetoken';
 
 const createdLogin = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
-  const { type, message } = await login(email, password);
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
+    const { type, message } = await login(email, password);
 
-  const token = jwt.sign({ message }, secret, { expiresIn: '7d', algorithm: 'HS256' });
-  if (type) return res.status(500).json(message);
+    const token = jwt.sign({ message }, secret, { expiresIn: '7d', algorithm: 'HS256' });
+    if (type) return res.status(500).json(message);
 
-  return res.status(200).json({ token });
+    return res.status(200).json({ token });
+  } catch (error: any) {
+    res.status(error.status).json({ message: error.message });
+  }
 };
 
 export default createdLogin;
--- app/backend/src/database/service/user.service.ts
+++ app/backend/src/database/service/user.service.ts
@@ -1,7 +1,19 @@
+import * as Joi from 'joi';
 import userModel from '../models/user.model';
+
+const schema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
 
 const login = async (email: string, password: string) => {
   const createdLogin = await userModel.findOne({ where: { email, password } });
+
+  const { error } = schema.validate({ email, password });
+  if (error || !createdLogin) {
+    const e: any = { status: 401, message: 'Invalid email or password' };
+    throw e;
+  }
   return { type: null, message: createdLogin };
 };
 

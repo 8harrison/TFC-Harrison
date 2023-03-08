f9152ed13c1085d2256805196e0f72f529d1dd30
Harrison <monteiro4100@gmail.com>
2023-03-08 12:30:03 -0300

    requisito 15

--- app/backend/src/database/controller/matches.controller.ts
+++ app/backend/src/database/controller/matches.controller.ts
@@ -1,12 +1,25 @@
 import { Request, Response } from 'express';
 import findAll from '../service/matches.service';
+import { findOne } from '../service/teams.service';
 
 const getAll = async (_req: Request, res: Response) => {
   const { type, message } = await findAll();
 
   if (type) return res.status(500).json(message);
-  const results = message.map((partida) => partida.dataValues);
-  return res.status(200).json(results);
+  const results = message.map(async (partida) => {
+    const findHomeTeam = await findOne(partida.homeTeamId);
+    const findAwayTeam = await findOne(partida.awayTeamId);
+
+    const retorno = {
+      ...partida.dataValues,
+      homeTeam: { teamName: findHomeTeam.message?.teamName },
+      awayTeam: { teamName: findAwayTeam.message?.teamName },
+    };
+    return retorno;
+  });
+  const resulted = await Promise.all(results);
+  console.log(resulted);
+  return res.status(200).json(resulted);
 };
 
 export default getAll;

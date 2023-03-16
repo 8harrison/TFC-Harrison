be34bfe3af98f4d5f35c2ef2f87fb75b4de6df14
Harrison <monteiro4100@gmail.com>
2023-03-16 08:49:57 -0300

    requisito 24 e 25

--- app/backend/src/database/service/leaderBoard.service.ts
+++ app/backend/src/database/service/leaderBoard.service.ts
@@ -21,31 +21,33 @@ const name = (matches: any, teams: any) => {
   return filterTeam;
 };
 
-const todoAqueleIf = (a: any, b: any) => {
-  if (a.name > b.name) {
-    return 1;
-  } if (a.name < b.name) {
-    return -1;
-  }
-};
-
 const balanceEEfficiency = (test: any) => {
   const obj = test;
   obj.goalsBalance = (test.goalsFavor - test.goalsOwn);
-  obj.efficiency = (test.totalVictories / test.totalGames) * 100;
+  obj.efficiency = ((test.totalPoints / (test.totalGames * 3)) * 100).toFixed(2);
   return (test.goalsBalance, test.efficiency);
+};
+
+const seVitoria = (test: any, match: any) => {
+  const obj = test;
+  obj.totalVictories += 1; obj.totalPoints += 3; obj.goalsFavor += match.homeTeamGoals;
+  obj.goalsOwn += match.awayTeamGoals;
+};
+
+const seEmpate = (test: any, match: any) => {
+  const obj = test;
+  obj.totalDraws += 1; obj.totalPoints += 1; obj.goalsFavor += match.homeTeamGoals;
+  obj.goalsOwn += match.awayTeamGoals;
 };
 const registerData = (matches: any, team: any, obj: objetoLeaderBoard) => {
   const test = obj;
   matches.forEach((match: any) => {
-    if (team.id === match.homeTeamId) {
+    if (team.id === match.homeTeamId && !match.inProgress) {
       test.totalGames += 1;
       if (match.homeTeamGoals > match.awayTeamGoals) {
-        test.totalVictories += 1; test.totalPoints += 3; test.goalsFavor += match.homeTeamGoals;
-        test.goalsOwn += match.awayTeamGoals;
+        seVitoria(test, match);
       } else if (match.homeTeamGoals === match.awayTeamGoals) {
-        test.totalDraws += 1; test.totalPoints += 1; test.goalsFavor += match.homeTeamGoals;
-        test.goalsOwn += match.awayTeamGoals;
+        seEmpate(test, match);
       } else {
         test.totalLosses += 1; test.goalsFavor += match.homeTeamGoals;
         test.goalsOwn += match.awayTeamGoals;
@@ -55,6 +57,15 @@ const registerData = (matches: any, team: any, obj: objetoLeaderBoard) => {
   });
   return test;
 };
+
+const sorts = (clubs: any) => {
+  const sorted = clubs.sort((a: any, b: any) => a.goalsOwn - b.goalsOwn)
+    .sort((a:any, b: any) => b.goalsFavor - a.goalsFavor)
+    .sort((a:any, b: any) => b.goalsBalance - a.goalsBalance)
+    .sort((a:any, b: any) => b.totalPoints - a.totalPoints);
+  return sorted;
+};
+
 const pointTotal = (matches: any, clubNames: any) => {
   const clubs = clubNames.map((team: any) => {
     const obj = {
@@ -72,8 +83,7 @@ const pointTotal = (matches: any, clubNames: any) => {
     const objeto = registerData(matches, team, obj);
     return objeto;
   });
-  return clubs.sort((a:any, b: any) => todoAqueleIf(a, b))
-    .sort((a:any, b: any) => b.totalPoints - a.totalPoints);
+  return sorts(clubs);
 };
 
 const leaderBoardHomeTeam = async () => {

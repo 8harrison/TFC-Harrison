321e1b388cf753bf1a18bec7c58f1c9e3053dd28
Harrison <monteiro4100@gmail.com>
2023-03-16 19:29:32 -0300

    requisito 14

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -7,11 +7,13 @@ import { app } from '../app';
 import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
-// import times from './team.controller.mock'
+import {times} from './team.controller.mock'
+import teamsModel from '../database/models/teams.model';
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXNzYWdlIjp7ImlkIjoyLCJ1c2VybmFtZSI6IlVzZXIiLCJyb2xlIjoidXNlciIsImVtYWlsIjoidXNlckB1c2VyLmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JFk4QWJpOGpYdnNYeXFtLnJtcDBCLnVRQkE1cVV6N1Q2R2hsZy9DdlZyL2dMeFlqNVVBWlZPIn0sImlhdCI6MTY3OTAwNDExNiwiZXhwIjoxNjc5NjA4OTE2fQ.hFOrzLVoE4tJb6CXpIlci4nHbAb0_XZuVM29ohRUHYo"
 describe('Seu teste', () => {
   /**
    * Exemplo do uso de stubs com tipos
@@ -19,21 +21,71 @@ describe('Seu teste', () => {
   let getTeams: any;
 
   // let chaiHttpResponse: Response;
-  before(async () => {
-    try {
-      getTeams = await chai.request(app)
-      .get('/teams')
-    } catch (error: any) {
-      console.log(error.message)
-    }
-    sinon.stub
-  describe('teste do requisito 2- testando endpoint teams', async() => {
-    it('retorna status 200', async () => {
-      const {status} = getTeams
-
-      expect(status).to.be.equal(200);
-    })
+//   before(async () => {
+//     // try {
+      
+//     // } catch (error: any) {
+//     //   console.log(error.message)
+//     // }
+//     // sinon.stub
+// })
+it('retorna status 200 /teams e retorna lista com times', async () => {
+  sinon.stub(teamsModel, 'findAll').resolves(times)
+  const result = await chai.request(app)
+  .get('/teams')
+  expect(result.status).to.be.equal(200);
+  expect(result.body).to.deep.equal(times);
+})
+it('retorna status 200 /logins e retorna login', async () => {
+  // sinon.stub(teamsModel, 'findAll').resolves(times)
+  const result = await chai.request(app)
+  .post('/login').send({
+    email: "user@user.com",
+    password: "secret_user",
   })
+  expect(result.status).to.be.equal(200);
+  // expect(result.body).to.deep.equal(times);
+})
+it('retorna 401 da rota login',async () => {
+  const result = await chai.request(app)
+  .post('/login').send({
+    email: "user@user",
+    password: "secret_user",
+  })
+  expect(result.status).to.be.equal(401)
+})
+
+it('retorna 401 do login/role', async () => {
+  const result = await chai.request(app)
+  .post('/login/role').send({
+    email: "user@user",
+    password: "secret_user",
+  })
+  expect(result.status).to.be.equal(404)
+})
+// it('retorna 200 com id', async() => {
+//   const id = 5;
+//   const result = await chai.request(app)
+//   .get(`/teams/${id}`)
+//   expect(result.status).to.be.equal(200)
+// })
+
+it('retorna 200 de /matches get', async () => {
+  const result = await chai.request(app)
+  .get('/matches')
+  expect(result.status).to.be.equal(200);
+})
+
+it('retorna 200 de /matches/:id', async () => {
+  const result = await chai.request(app)
+  .patch('/matches').set('Authorization',  `Bearer ${TOKEN}` )
+  expect(result.status).to.be.equal(200);
+})
+
+it('retorna 200 de /matches post', async () => {
+  const result = await chai.request(app)
+  .post('/matches').set('Authorization', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXNzYWdlIjp7ImlkIjoyLCJ1c2VybmFtZSI6IlVzZXIiLCJyb2xlIjoidXNlciIsImVtYWlsIjoidXNlckB1c2VyLmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JFk4QWJpOGpYdnNYeXFtLnJtcDBCLnVRQkE1cVV6N1Q2R2hsZy9DdlZyL2dMeFlqNVVBWlZPIn0sImlhdCI6MTY3OTAwNDExNiwiZXhwIjoxNjc5NjA4OTE2fQ.hFOrzLVoE4tJb6CXpIlci4nHbAb0_XZuVM29ohRUHYo")
+  expect(result.status).to.be.equal(200);
 })
   // before(async () => {
   //   sinon
---
+++ app/backend/src/tests/team.controller.mock.ts
@@ -0,0 +1,18 @@
+import teamsModel from '../database/models/teams.model';
+import teamModel from '../database/models/teams.model';
+const times: teamsModel[] = [
+    {
+      "id": 1,
+      "teamName": "Avaí/Kindermann"
+    },
+    {
+      "id": 2,
+      "teamName": "Bahia"
+    },
+    {
+      "id": 3,
+      "teamName": "Botafogo"
+    },
+] as teamsModel[];
+
+export { times };
